acc1942a08ae8365c0ccaaa22622724798c46832
olyao23 <101214018+olyao23@users.noreply.github.com>
2022-05-09 13:49:19 -0200

    Added App file and made changes to the TypographyContent component

---
+++ react-app/src/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainCard from "../Card/mainCard";
+import styles from "./app.module.css";
+
+function App() {
+  return (
+    <div className={styles.appJsStyle}>
+      <MainCard />
+    </div>
+  );
+}
+
+export default App;
