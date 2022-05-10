ab0a17f3115ed70d9a4dfde8f5b4bd698d2010c0
olyao23 <101214018+olyao23@users.noreply.github.com>
2022-05-10 16:49:47 -0200

    Made changes to typography

--- react-app/src/Typography/typographyContent.js
+++ react-app/src/Typography/typographyContent.js
@@ -4,22 +4,18 @@ import styles from "./typography.module.css";
 function TypographyContent() {
   function Typography(props) {
     if (props.variant === "heading") {
-      props.className = styles.headerStyle;
-      //props.className="headerStyle";
-      return <h1>{props.children}</h1>;
+      return <h1 className={styles.headerStyle}>{props.children}</h1>;
     }
 
     if (props.variant === "body") {
-      props.className = styles.bodyStyle;
-      //props.className="bodyStyle";
-      return <p>{props.children}</p>;
+      return <p className={styles.bodyStyle}>{props.children}</p>;
     }
   }
 
   return (
     <div>
-      <Typography>Order summary</Typography>
-      <Typography>
+      <Typography variant="heading">Order summary</Typography>
+      <Typography variant="body">
         You can now listen to millions of songs, audiobooks, and podcasts on any
         device anywhere you like!
       </Typography>
